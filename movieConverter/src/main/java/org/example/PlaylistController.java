a7980b3366a73d2800d13f121e1a711ee4121053
Willian Lima <willian.slima@mercadolivre.com>
2024-06-19 10:41:34 -0300

    init esqueleto projeto

---
+++ movieConverter/src/main/java/org/example/PlaylistController.java
@@ -0,0 +1,58 @@
+package org.example;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.web.bind.annotation.*;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@RestController
+@RequestMapping("/api")
+@EnableAsync
+public class PlaylistController {
+    @Autowired
+    private PlaylistService playlistService;
+    @PostMapping("/convertPlaylist")
+    public String convertPlaylistToMp3(@RequestParam String playlistUrl) {
+        try {
+            List<VideoInfo> videoUrls = playlistService.getPlaylistVideos(playlistUrl);
+            for (VideoInfo videoUrl : videoUrls) {
+               // playlistService.convertVideoToMp3(videoUrl);
+            }
+            return "Playlist conversion to MP3 started.";
+        } catch (Exception e) {
+            e.printStackTrace();
+            return "Error converting playlist: " + e.getMessage();
+        }
+    }
+
+    private List<String> getPlaylistVideoUrls(String playlistUrl) throws Exception {
+        String command = "yt-dlp --flat-playlist --get-id " + playlistUrl;
+        ProcessBuilder pb = new ProcessBuilder("bash", "-c", command);
+        Process process = pb.start();
+
+        BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
+        List<String> videoIds = reader.lines().collect(Collectors.toList());
+        process.waitFor();
+
+        return videoIds.stream()
+                .map(id -> "https://www.youtube.com/watch?v=" + id)
+                .collect(Collectors.toList());
+    }
+
+    private void convertVideoToMp3(String videoUrl) throws Exception {
+        String command = String.format("yt-dlp -x --audio-format mp3 --audio-quality 192K --output \"downloaded_videos/%%(title)s.%%(ext)s\" %s", videoUrl);
+
+        ProcessBuilder pb = new ProcessBuilder("bash", "-c", command);
+        Process process = pb.start();
+
+        BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
+        String line;
+        while ((line = reader.readLine()) != null) {
+            System.out.println(line);
+        }
+        process.waitFor();
+    }
+}
---
+++ movieConverter/src/main/java/org/example/VideoInfo.java
@@ -0,0 +1,11 @@
+package org.example;
+
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
+public class VideoInfo {
+    private String videoId;
+    private String playlistId;
+    private String url;
+    private int index;
+}
